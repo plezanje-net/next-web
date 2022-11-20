e0fdc96128d2f41778c9e3bab67d32c0066df642
Nejc Stebe <nejc.stebe92@gmail.com>
2022-11-20 16:29:24 +0100

    Add reusable button component

---
+++ src/pages/sandbox/buttons.tsx
@@ -0,0 +1,18 @@
+import Button from "../../components/ui/Button";
+
+export default function Buttons() {
+  return (
+    <>
+      <h1>Our buttons</h1>
+      <div className="flex flex-col items-start p-3">
+        <Button variant="primary" className="mb-2">
+          Button primary
+        </Button>
+        <Button variant="secondary" className="mb-2">
+          Button secondary
+        </Button>
+        <Button loading>Button 3</Button>
+      </div>
+    </>
+  );
+}
