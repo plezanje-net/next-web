ecb33fd0e64ced16cc456fc059a184206f22e703
Anze Demsar <demshy@gmail.com>
2022-11-20 15:47:43 +0100

    add layout component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { AuthProvider } from "../utils/providers/auth-provider";
 import App from "next/app";
 import cookies from "next-cookies";
 import { NextPageContext } from "next";
+import Layout from "../components/layout/layout";
 
 type MyAppProps = AppProps & { authToken: string };
 
@@ -14,35 +15,9 @@ function MyApp({ Component, pageProps, authToken }: MyAppProps) {
   return (
     <>
       <AuthProvider token={authToken}>
-        <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <meta name="apple-mobile-web-app-title" content="Plezanje.net" />
-          <meta name="application-name" content="Plezanje.net" />
-          <meta name="msapplication-TileColor" content="#ffc40d" />
-          <meta name="msapplication-config" content="/browserconfig.xml" />
-          <meta name="theme-color" content="#ffffff" />
-        </Head>
-
-        <Component {...pageProps} />
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
       </AuthProvider>
     </>
   );
