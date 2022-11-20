c0d225d877edcfc2ea98bb1b4cba6560703bdf2c
salamca <tomaz.bevk@gmail.com>
2022-11-20 14:17:48 +0100

    Set some tailwind theme vars.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,49 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ["Poppins", "sans-serif"],
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+    fontSize: {
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["20px", "28px"],
+      xl: ["25px", "32px"],
+      "2xl": ["31px", "38px"],
+      "3xl": ["39px", "48px"],
+    },
+    fontWeight: {
+      normal: 400,
+      medium: 500,
+    },
+    colors: {
+      white: "#ffffff",
+      blue: {
+        100: "#aaccf0",
+        // 200: "...",  // will be added if/when needed
+        // 300: "#...",
+        // 400: "#...",
+        500: "#2b7fd9",
+        600: "#246cb8",
+        700: "#1e5998",
+        // 800: "#...",
+        // 900: "#...",
       },
+      neutral: {
+        100: "#f5f5f5",
+        200: "#e5e5e5",
+        300: "#d4d4d4",
+        400: "#a3a3a3",
+        500: "#737373",
+        600: "#525252",
+        700: "#404040",
+        800: "#262626",
+        900: "#171717",
+      },
+      red: {
+        500: "#D9422B",
+      },
+      // ...
     },
   },
   plugins: [],
