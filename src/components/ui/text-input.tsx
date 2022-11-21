54c289e77743bcf2901a4b741019f81a3c875464
salamca <tomaz.bevk@gmail.com>
2022-11-21 19:53:04 +0100

    Add text input component and a sandbox page showcasing its usage

---
+++ src/components/ui/text-input.tsx
@@ -0,0 +1,71 @@
+import { ReactNode, useRef, useState } from "react";
+import { useTextField, useFocus, AriaTextFieldOptions } from "react-aria";
+
+type Props = {
+  label?: string;
+  description?: string;
+  errorMessage?: string;
+  disabled?: boolean;
+  prefix?: ReactNode;
+  suffix?: ReactNode;
+} & AriaTextFieldOptions<"input">;
+
+function TextInput(props: Props) {
+  const { isDisabled, label, description, errorMessage, prefix, suffix } =
+    props;
+  const inputRef = useRef(null);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const { labelProps, inputProps, descriptionProps, errorMessageProps } =
+    useTextField(props, inputRef);
+
+  const { focusProps } = useFocus({
+    onFocusChange: (isFocused) => setIsFocused(isFocused),
+  });
+
+  return (
+    <div>
+      {label && (
+        <label {...labelProps} className="block">
+          {label}
+        </label>
+      )}
+      <div
+        className={`flex items-center rounded-lg border border-neutral-400 focus:ring focus:ring-blue-100
+                    ${isDisabled && "bg-neutral-100 text-neutral-400"}
+                    ${isFocused && "ring ring-blue-100"}
+                    ${label && "mt-1"}
+                    ${errorMessage && "border-red-500"}
+                  `}
+      >
+        {prefix && <div className="mx-2">{prefix}</div>}
+
+        <input
+          {...inputProps}
+          {...focusProps}
+          ref={inputRef}
+          className={`flex-1 rounded-lg py-2 placeholder:text-neutral-400 focus:outline-none
+                      ${!prefix && "pl-4"}
+                      ${!suffix && "pr-4"}
+                    `}
+        />
+
+        {suffix && <div className="mx-2">{suffix}</div>}
+      </div>
+
+      {description && !errorMessage && (
+        <div {...descriptionProps} className="mt-1 text-sm">
+          {description}
+        </div>
+      )}
+
+      {errorMessage && (
+        <div {...errorMessageProps} className="mt-1 text-sm text-red-500">
+          {props.errorMessage}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default TextInput;
