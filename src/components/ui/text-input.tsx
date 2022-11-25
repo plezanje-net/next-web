4913266ef1086e5871c7e7741514fcd48700e7de
salamca <tomaz.bevk@gmail.com>
2022-11-25 11:02:18 +0100

    Fix colors on text input disabled state.

--- src/components/ui/text-input.tsx
+++ src/components/ui/text-input.tsx
@@ -32,7 +32,10 @@ function TextInput(props: Props) {
       )}
       <div
         className={`flex items-center rounded-lg border border-neutral-400 focus:ring focus:ring-blue-100
-                    ${isDisabled && "bg-neutral-100 text-neutral-400"}
+                    ${
+                      isDisabled &&
+                      "border-neutral-300 bg-neutral-100 text-neutral-400"
+                    }
                     ${isFocused && "ring ring-blue-100"}
                     ${label && "mt-1"}
                     ${errorMessage && "border-red-500"}
