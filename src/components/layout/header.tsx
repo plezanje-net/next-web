5410e246c5ee1d062892df42701b6da6d12399c6
salamca <tomaz.bevk@gmail.com>
2022-12-02 15:10:20 +0100

    Remove bg colors, add links, clean up.

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -35,128 +35,84 @@ function Header() {
     },
   ];
 
-  const [menuOpened, setMenuOpened] = useState(true);
-
-  console.log(authCtx);
-
-  const search = <IconSearch />;
-  // if (authCtx) {
-  //   loginLink = "Prijava";
-  // }
-  // const loginOrUserName = authCtx.status ? "Slavko Majonezič" : "Prijava";
-
-  const loggedIn = !!authCtx.status;
-
-  // TODO: get users full name from authCtx ?
-  const userLink = "Slavko Majonezič";
-  const userLinkShort = "Slavko Majonezič"
-    .split(" ")
-    .reduce((prev, curr) => prev + curr[0], "");
+  const [menuOpened, setMenuOpened] = useState(false);
 
   const handleMenuClick = () => {
     setMenuOpened(!menuOpened);
   };
 
+  const loggedIn = !!authCtx.status;
+
+  // TODO: get user's full name from authCtx. adjust BE ?
+  const userFullName = "Slavko Majonezič"; //TODO: replace testing dummy
+  const userFullNameShort = userFullName
+    .split(" ")
+    .reduce((prev, curr) => prev + curr[0], "")
+    .toUpperCase();
+
   return (
     <header className="flex flex-col justify-between border-b border-b-neutral-200 px-4 xs:px-8 lg:flex-row lg:items-center">
       {/* Logo row */}
-      <div className="box-content flex h-20 items-center justify-between ">
+      <div className="box-content flex h-20 items-center justify-between">
         {/* Logo */}
-        <div className="-ml-2 bg-[pink] py-1 px-2">
-          <div className="bg-[coral]">
-            <Logo />
-          </div>
-        </div>
+        <Link className="-ml-2 py-1 px-2" href="/">
+          <Logo />
+        </Link>
+
+        {/* Right part of logo row */}
         <div className="flex">
-          <div className="hidden bg-[lightgreen] p-2 xs:block lg:hidden">
-            <div className="bg-[darkcyan]">{search}</div>
+          <div className="hidden cursor-pointer  p-2 xs:block lg:hidden">
+            <IconSearch />
           </div>
+
           {loggedIn && (
-            <div className="bg-[magenta] p-2 lg:hidden">{userLinkShort}</div>
+            <Link className="p-2 xs:ml-4 lg:hidden" href="/sandbox/auth">
+              {userFullNameShort}
+            </Link>
           )}
+
           <div
-            className="-mr-2 bg-[violet] p-2 lg:hidden"
+            className="-mr-2 cursor-pointer p-2 xs:ml-4 lg:hidden"
             onClick={handleMenuClick}
           >
-            <div className="bg-[darkcyan]">
-              {menuOpened ? <IconClose /> : <IconMenu />}
-            </div>
+            {menuOpened ? <IconClose /> : <IconMenu />}
           </div>
         </div>
       </div>
 
+      {/* dropdown menu when <lg */}
       <div
-        className={`bg-[salmon] pb-4 lg:flex lg:pb-0 ${
-          menuOpened ? "block" : "hidden"
-        }`}
+        className={`pb-4 lg:flex lg:pb-0 ${menuOpened ? "block" : "hidden"}`}
       >
-        <div className="-mx-2 bg-[#6ab26a] p-2 xs:hidden lg:mx-0 lg:block">
-          <div className="bg-[floralwhite]">{search}</div>
+        <div className="-mx-2 cursor-pointer p-2 xs:hidden lg:mx-0 lg:block">
+          <IconSearch />
         </div>
 
         {!loggedIn && (
-          <div className="-mx-2 bg-[beige] p-2 lg:hidden">
-            <div className="bg-[deepskyblue]">Prijava</div>
-          </div>
+          <Link className="-mx-2 block p-2 lg:hidden" href="/sandbox/auth">
+            Prijava
+          </Link>
         )}
 
         {navLinks.map((link) => (
           <Link
             key={link.href}
             href={link.href}
-            className={`-mx-2 block bg-[orange] p-2 lg:mx-0 lg:ml-4
+            className={`-mx-2 block p-2 lg:mx-0 lg:ml-4
                         ${link.isActive && "text-blue-500"}`}
           >
-            <div className="bg-[purple]">{link.label}</div>
+            {link.label}
           </Link>
         ))}
 
-        <div className="-mr-2 hidden bg-[beige] p-2 py-2 lg:ml-4 lg:block">
-          <div className="bg-[burlywood]">
-            {loggedIn ? userLink : "Prijava"}
-          </div>
-        </div>
+        <Link
+          className="-mr-2 hidden p-2 py-2 lg:ml-4 lg:block"
+          href="/sandbox/auth"
+        >
+          {loggedIn ? userFullName : "Prijava"}
+        </Link>
       </div>
     </header>
-
-    // <header className="box-content flex flex-col border-b border-b-neutral-200 px-2 sm:px-4">
-    //   <div className="flex h-20 items-center justify-between">
-    //     <Link className="px-2 py-2 sm:px-4" href="/">
-    //       <Logo />
-    //     </Link>
-
-    //     <div className="flex">
-    //       <span className="hidden px-4 py-2 sm:block">
-    //         <IconSearch />
-    //       </span>
-    //       {authCtx.status && (
-    //         <span className="px-2 py-2 font-medium sm:px-4 lg:hidden">SM</span>
-    //       )}
-    //       <span className="px-2 py-2 sm:px-4 lg:hidden">
-    //         {menuOpened ? <IconClose /> : <IconMenu />}
-    //       </span>
-    //     </div>
-    //   </div>
-
-    //   <nav className={`flex flex-col`}>
-    //     {navLinks.map((link) => (
-    //       <Link
-    //         key={link.href}
-    //         href={link.href}
-    //         className={`inline-block px-2 py-2 font-medium sm:px-4
-    //                     ${link.isActive && "text-blue-500"}`}
-    //       >
-    //         {link.label}
-    //       </Link>
-    //     ))}
-    //     <Link
-    //       href="/sandbox/auth"
-    //       className="inline-block px-2 py-2 font-medium sm:px-4"
-    //     >
-    //       {authCtx.status ? "Slavko Majonezič" : "Prijava"}
-    //     </Link>
-    //   </nav>
-    // </header>
   );
 }
 
