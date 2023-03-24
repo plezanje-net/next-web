d76b0f23e8e4907bf22dda516afadc95c9057e11
salamca <tomaz.bevk@gmail.com>
2023-03-24 16:18:03 +0100

    Add multi option to select input, fix icons positioning and update sandbox demo with examples.

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,5 +1,5 @@
 import { Listbox } from "@headlessui/react";
-import { cloneElement, ReactElement } from "react";
+import { Children, cloneElement, ReactElement, ReactNode } from "react";
 import IconCheck from "./icons/check";
 import IconExpand from "./icons/expand";
 
@@ -11,17 +11,18 @@ interface OptionProps {
   icon?: ReactElement;
 }
 
-function Option(props: OptionProps) {
+function Option({ children, id, value, disabled, icon }: OptionProps) {
   return (
     <Listbox.Option
-      key={props.id}
-      value={props.value}
-      disabled={props.disabled}
+      key={id}
+      value={value}
+      disabled={disabled}
       className="flex cursor-pointer justify-between py-2 pl-4 pr-2 ui-selected:text-blue-500 ui-active:bg-neutral-100 ui-active:text-blue-500 ui-disabled:cursor-default ui-disabled:text-neutral-400"
     >
-      <span className="flex items-center justify-start gap-1">
-        {props.icon && cloneElement(props.icon, { size: "small" })}
-        {props.children}
+      <span>
+        {icon &&
+          cloneElement(icon, { size: "small", className: "mb-1 inline mr-1" })}
+        {children}
       </span>
       <IconCheck className="hidden text-neutral-900 ui-selected:block" />
     </Listbox.Option>
@@ -30,11 +31,20 @@ function Option(props: OptionProps) {
 
 interface SelectProps {
   children: ReactElement<OptionProps>[];
+  defaultValue?: string | string[];
+  label: string;
+  placeholder?: string;
+  multi?: boolean;
 }
 
-function Select(props: SelectProps) {
-  const { children } = props;
-
+function Select({
+  children,
+  defaultValue,
+  label,
+  placeholder,
+  multi,
+}: SelectProps) {
+  // map options (children) to a temporary 'associative array' to be able to access labels an icons later
   let options: {
     [key: string]: {
       label: string;
@@ -42,30 +52,59 @@ function Select(props: SelectProps) {
       icon?: ReactElement;
     };
   } = {};
-  children.map((option) => {
+  Children.map(children, (option) => {
     options[option.props.value] = {
       value: option.props.value,
       label: option.props.children,
       ...(option.props.icon && {
-        icon: cloneElement(option.props.icon, { size: "small" }),
+        icon: cloneElement(option.props.icon, {
+          size: "small",
+          className: "mb-1 inline mr-1",
+        }),
       }),
     };
   });
 
+  const getOptionLabel = (selected: string | string[]) => {
+    // the selected value is eiter a single value or an array (if this is a mutliselect)
+    if (multi) {
+      return (selected as string[]).map((value: string, index) => (
+        <span key={value}>
+          {options[value].icon}
+          {options[value].label}
+          {index < selected.length - 1 && <>, </>}
+        </span>
+      ));
+    } else {
+      return (
+        <>
+          {options[selected as string].icon}
+          {options[selected as string].label}
+        </>
+      );
+    }
+  };
+
   return (
-    <Listbox defaultValue="" as="div" className="w-80">
-      <Listbox.Button className="flex w-80 justify-between rounded-lg border border-neutral-400 py-2 pl-4 pr-2 focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-100">
+    <Listbox
+      defaultValue={defaultValue}
+      as="div"
+      className="w-80"
+      multiple={multi}
+    >
+      <Listbox.Label>{label}</Listbox.Label>
+      <Listbox.Button className="mt-1 flex w-80 justify-between gap-2 rounded-lg border border-neutral-400 py-2 pl-4 pr-2 focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-100">
         {({ value }) => (
           <>
-            {value ? (
-              <span className="flex items-center justify-start gap-1">
-                {options[value].icon}
-                <span>{options[value].label}</span>
+            {!!value?.length ? (
+              <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+                {getOptionLabel(value)}
               </span>
             ) : (
-              <span className="text-neutral-400">Izberi možnost</span>
+              <span className="text-neutral-400">{placeholder}</span>
             )}
-            <IconExpand />
+
+            <IconExpand className="shrink-0" />
           </>
         )}
       </Listbox.Button>
