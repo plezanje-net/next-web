fa4d02700e79d567c7e8414019d99dd466c99f2d
Anze Demsar <demshy@gmail.com>
2023-03-31 20:38:12 +0200

    expand multiple sectors and save state to route

--- src/components/crag/crag-table.tsx
+++ src/components/crag/crag-table.tsx
@@ -1,12 +1,5 @@
 import { useRouter } from "next/router";
-import {
-  createContext,
-  ReactNode,
-  useEffect,
-  useLayoutEffect,
-  useRef,
-  useState,
-} from "react";
+import { createContext, ReactNode, useEffect, useRef, useState } from "react";
 import { gql, useQuery } from "urql";
 import {
   Crag,
@@ -162,7 +155,6 @@ const CragTableColumns: CragTableColumn[] = [
 
 function CragTable({ crag }: Props) {
   const router = useRouter();
-  const selectedSector = parseInt(router.query.sector as string) ?? null;
 
   const [state, setState] = useState<CragTableState>({
     compact: true,
@@ -175,13 +167,6 @@ function CragTable({ crag }: Props) {
 
   const [compact, setCompact] = useState(true);
   const [breakpoint, setBreakpoint] = useState(500);
-
-  const toggleSector = (index: number) => {
-    router.push({
-      pathname: `/plezalisce/${crag.slug}`,
-      ...(selectedSector == index + 1 ? {} : { query: { sector: index + 1 } }),
-    });
-  };
 
   // Load user's crag summary if logged in and after server-side render
   const [ascents, setAscents] = useState<Map<string, string>>(new Map());
@@ -243,6 +228,69 @@ function CragTable({ crag }: Props) {
     setState((state) => ({ ...state, search: debouncedSearch }));
   }, [debouncedSearch]);
 
+  // Sectors collapse/expand
+  // get initial state from query params (could be empty, string or array)
+  const [expandedSectors, setExpandedSectors] = useState<number[]>(
+    router.query.sektor
+      ? typeof router.query.sektor == "string"
+        ? [parseInt(router.query.sektor)]
+        : router.query.sektor.map((s: string) => parseInt(s))
+      : []
+  );
+
+  // toggle sector handler update state and silently push it to router
+  const toggleSector = (index: number) => {
+    setExpandedSectors((state) => {
+      const i = state.indexOf(index);
+      if (i === -1) {
+        state.push(index);
+      } else {
+        state.splice(i, 1);
+      }
+      return state;
+    });
+
+    router.push(
+      {
+        pathname: `/plezalisce/${crag.slug}`,
+        ...(expandedSectors.length == 0
+          ? {}
+          : { query: { sektor: expandedSectors } }),
+      },
+      undefined,
+      { scroll: false, shallow: true }
+    );
+  };
+
+  // observe expanded sectors and always set the anchor for the last visible sector on screen
+
+  // I don't really like this anymore but it should work
+
+  // const [lastVisibleSector, setLastVisibleSector] = useState(-1);
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver((entries) => {
+  //     let firstVisibleSectorFound = false;
+  //     entries.forEach((entry) => {
+  //       if (entry.isIntersecting) {
+  //         const sectorIndex = parseInt(entry.target.id.split("-")[1]);
+  //         setLastVisibleSector(sectorIndex);
+  //       }
+  //     });
+  //   });
+  //   expandedSectors.forEach((index) => {
+  //     const sectorAnchor = document.getElementById(`sektor-${index}`);
+  //     if (sectorAnchor) {
+  //       observer.observe(sectorAnchor);
+  //     }
+  //   });
+  // }, [expandedSectors]);
+
+  // useEffect(() => {
+  //   if (lastVisibleSector != -1) {
+  //     window.location.hash = `#sektor-${lastVisibleSector}`;
+  //   }
+  // }, [lastVisibleSector]);
+
   return (
     <div ref={containerRef}>
       <CragTableContext.Provider value={{ state, setState }}>
@@ -261,9 +309,7 @@ function CragTable({ crag }: Props) {
           </div>
         </div>
         <div className="container mx-auto mt-4 sm:px-8">
-          {router.query.combine ||
-          state.search != "" ||
-          crag.sectors.length == 1 ? (
+          {router.query.combine || state.search || crag.sectors.length == 1 ? (
             <CragRoutes
               crag={crag}
               routes={crag.sectors.reduce(
@@ -280,11 +326,12 @@ function CragTable({ crag }: Props) {
                   index > 0 ? "border-t border-t-neutral-200" : ""
                 }`}
               >
+                {/* <a id={`sektor-${index}`} /> */}
                 <CragSector
                   crag={crag}
                   sector={sector as Sector}
                   ascents={ascents}
-                  isOpen={index + 1 == selectedSector}
+                  isOpen={expandedSectors.includes(index)}
                   onToggle={() => toggleSector(index)}
                 />
               </div>
