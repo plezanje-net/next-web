ff7729ed0b121bd4b07d1508884e15439ea6ce80
salamca <tomaz.bevk@gmail.com>
2023-04-14 15:28:22 +0200

    Add a wrap around options and pad the wrap for the case of select being last elt on page (needs some space below).

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -120,11 +120,15 @@ function Select({
       )}
 
       <Listbox.Options
-        className={`absolute z-10 mt-2 overflow-hidden rounded-lg border border-neutral-400 bg-white focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-100 ${
+        className={`absolute z-10 pb-2 ${
           customTrigger ? "w-auto whitespace-nowrap" : "w-full"
         }`}
       >
-        {children}
+        <div
+          className={`mt-2 overflow-hidden rounded-lg border border-neutral-400 bg-white focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-100`}
+        >
+          {children}
+        </div>
       </Listbox.Options>
     </Listbox>
   );
