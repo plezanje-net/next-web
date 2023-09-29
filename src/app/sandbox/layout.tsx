982c8fbc28bc9ea3a0a73531b9b861c1206b5ce8
salamca <tomaz.bevk@gmail.com>
2023-09-29 11:04:25 +0200

    Adjust sandbox layout to get fluid containers up to 2xl.

--- src/app/sandbox/layout.tsx
+++ src/app/sandbox/layout.tsx
@@ -2,13 +2,13 @@ function SandboxLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
       <div className="bg-neutral-100">
-        <div className="container mx-auto px-8">
+        <div className="mx-auto px-4 2xl:container xs:px-8">
           <div className="pt-4">
             <h1 className="pb-8 pt-12 text-3xl">Component showcase</h1>
           </div>
         </div>
       </div>
-      <div className="container mx-auto">{children}</div>
+      <div className="mx-auto 2xl:container">{children}</div>
     </>
   );
 }
