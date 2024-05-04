c4453bd21fe807462e5024f5665e4e67c3ed8c62
Anze Demsar <demshy@gmail.com>
2024-05-04 14:57:53 +0200

    disable image selection and fix auto width

--- src/app/[lang]/crag/[cragSlug]/gallery/components/image-list/image-list-slide.tsx
+++ src/app/[lang]/crag/[cragSlug]/gallery/components/image-list/image-list-slide.tsx
@@ -54,7 +54,7 @@ function ImageSlide({
           alt={`${image.title}`}
           quality={100}
           priority
-          className="mx-auto h-full w-auto object-contain"
+          className="mx-auto h-full select-none object-contain"
           onDoubleClick={toggleFullScreen}
           onTouchStart={handleTouchStart}
           onTouchEnd={handleTouchEnd}
