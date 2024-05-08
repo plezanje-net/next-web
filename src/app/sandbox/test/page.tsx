d613a54244dff265cbb04648fe6bcac6b4255f1c
salamca <tomaz.bevk@gmail.com>
2024-05-08 10:35:56 +0200

    wip and tests.

---
+++ src/app/sandbox/test/page.tsx
@@ -0,0 +1,7 @@
+import Test from "@/components/ui/test";
+
+function testPage() {
+  return <Test />;
+}
+
+export default testPage;
---
+++ src/components/ui/icons/left.tsx
@@ -0,0 +1,14 @@
+function IconLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      height="24"
+      width="24"
+      className="fill-current"
+    >
+      <path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z" />
+    </svg>
+  );
+}
+export default IconLeft;
