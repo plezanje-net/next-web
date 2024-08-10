ace96991d3fa3f1e7afd26867a34d76b107f9aa2
salamca <tomaz.bevk@gmail.com>
2024-08-10 20:16:43 +0200

    Condense date-picker on very small screens.

--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -572,7 +572,7 @@ function DatePicker({
         <FocusTrap focusTrapOptions={{ allowOutsideClick: true }}>
           <div
             ref={calendarPaneRef}
-            className="absolute mt-2 w-[296px] rounded-lg border border-neutral-400 bg-white px-2 py-3"
+            className="absolute mt-2 w-[256px] min-[400px]:w-[296px] rounded-lg border border-neutral-400 bg-white px-2 py-3"
           >
             <div className="flex justify-between">
               {/* month shuffler */}
@@ -643,7 +643,7 @@ function DatePicker({
               <div className="flex">
                 {["P", "T", "S", "Č", "P", "S", "N"].map((dayName, i) => (
                   <Fragment key={i}>
-                    <div className="flex h-10 w-10 items-center justify-center">
+                    <div className="flex h-[34px] w-[34px] min-[400px]:h-10 min-[400px]:w-10 items-center justify-center">
                       {dayName}
                     </div>
                   </Fragment>
@@ -887,9 +887,9 @@ function Day({
   return (
     <>
       {state == "empty" ? (
-        <div className="h-10 w-10"></div>
+        <div className="w-[34px] h-[34px] min-[400px]:w-10 min-[400px]:h-10"></div>
       ) : (
-        <div className="h-10 w-10 p-1">
+        <div className="w-[34px] h-[34px] min-[400px]:w-10 min-[400px]:h-10 p-1">
           <button
             className={`flex h-full w-full cursor-pointer items-center justify-center rounded-full text-center align-middle focus-visible:outline-none focus-visible:ring focus-visible:ring-blue-100 ${stateClasses}`}
             onClick={onClick}
