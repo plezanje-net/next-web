148b8d00b213562e3d83846f302e94297acbbaa6
Anze Demsar <demshy@gmail.com>
2024-10-01 07:54:01 +0200

    wip: pagination

--- src/app/[lang]/climbing-log/ascents/page.tsx
+++ src/app/[lang]/climbing-log/ascents/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+
+import Pagination from "@/components/ui/pagination";
+
 function ClimbingLogPage() {
+
+  function handlePageChange () {
+    console.log('page changed');
+  }
+
   return (
     <>
       Ascents
+      <Pagination currentPage={1} totalPages={100} onPageChange={handlePageChange} />
     </>
   );
 }
