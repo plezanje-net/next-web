fe98805c326f1a0bbf484fa5e45bf200653d2525
salamca <tomaz.bevk@gmail.com>
2024-10-18 16:50:31 +0200

    Add some icons and update icons sandbox.

---
+++ src/components/ui/icons/publish.tsx
@@ -0,0 +1,15 @@
+function IconPublish() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 -960 960 960"
+      width="24"
+      height="24"
+      className="fill-current"
+    >
+      <path d="M440-160v-326L336-382l-56-58 200-200 200 200-56 58-104-104v326h-80ZM160-600v-120q0-33 23.5-56.5T240-800h480q33 0 56.5 23.5T800-720v120h-80v-120H240v120h-80Z" />
+    </svg>
+  );
+}
+
+export default IconPublish;
