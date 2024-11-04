c720d14c0f82d608c0a95842fa22da7d8b22f5cf
Anze Demsar <demshy@gmail.com>
2024-11-04 10:20:56 +0100

    wip: rework datepicker to work with headless ui popover

--- src/app/sandbox/date-picker/page.tsx
+++ src/app/sandbox/date-picker/page.tsx
@@ -22,15 +22,15 @@ function DatePickerPage() {
   return (
     <div>
       <div className="relative mx-auto mt-8 w-80">
-        A disabled date picker
-        <div className="mt-2">
-          <DatePicker value={value2} onChange={setValue2} disabled />
-        </div>
-      </div>
-      <div className="relative mx-auto mt-8 w-80">
         A regular datepicker
         <div className="mt-2">
           <DatePicker value={value1} onChange={setValue1} />
+        </div>
+      </div>
+      <div className="relative mx-auto mt-8 w-80">
+        A disabled date picker
+        <div className="mt-2">
+          <DatePicker value={value2} onChange={setValue2} disabled />
         </div>
       </div>
     </div>
