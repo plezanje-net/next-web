14cc44de0c2c6ab86977d0bacd43ca4788b801d0
zoranmesec <zoran.mesec@gmail.com>
2024-11-18 23:30:01 +0100

    - bugfixes, code refactoring, linting

--- src/components/image-upload/image-upload.tsx
+++ src/components/image-upload/image-upload.tsx
@@ -1,22 +1,89 @@
-"use client"
+"use client";
 import Button from "@/components/ui/button";
 import Dialog, { DialogSize, DialogTitleSize } from "@/components/ui/dialog";
-import { ChangeEvent, FormEvent, ReactElement, useEffect, useRef, useState } from "react";
+import {
+  ChangeEvent,
+  ReactElement,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import Checkbox from "../ui/checkbox";
 import TextField from "../ui/text-field";
-import createImageAction from "./server-actions/create-image-action";
 import { useRouter } from "next/navigation";
-import Image from 'next/image';
+import Image from "next/image";
 import { AuthStatus } from "@/utils/auth/auth-status";
 import ProgressBar from "../ui/progress-bar";
 import { bytesToSize } from "@/utils/file-size";
 import IconClose from "../ui/icons/close";
 
+type TImageUploadResponse = {
+  aspectRatio: number;
+  author: string;
+  created: string;
+  description: string | null;
+  extension: string;
+  id: string;
+  legacy: string | null;
+  maxIntrinsicWidth: number;
+  path: string;
+  title: string;
+  updated: string;
+  userId: string;
+  __crag__: unknown;
+  __has_crag__: boolean;
+  __has_user__: boolean;
+  __user__: unknown;
+};
+
+async function createImageAction(
+  token: string,
+  formData: FormData,
+  progressCallback: (percentage: number) => void
+) {
+  return new Promise<TImageUploadResponse>(function (resolve, reject) {
+    if (!token) reject({ status: 401, statusText: "Missing token" });
+
+    try {
+      var xhr = new XMLHttpRequest();
+      xhr.open("POST", `${process.env.NEXT_PUBLIC_UPLOAD_URL}/image`, true);
+      xhr.setRequestHeader("authorization", token ? `Bearer ${token}` : "");
+      xhr.upload.onerror = () => {
+        reject({
+          status: xhr.status,
+          statusText: xhr.statusText,
+        });
+      };
+
+      xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 201) {
+          resolve(xhr.response as TImageUploadResponse);
+        } else if (xhr.readyState === XMLHttpRequest.DONE) {
+          reject({
+            status: xhr.status,
+            statusText: xhr.statusText,
+          });
+        }
+      };
+
+      xhr.upload.onprogress = (e) => {
+        const percentComplete = e.loaded / e.total;
+        progressCallback(percentComplete);
+      };
+      xhr.send(formData);
+    } catch (error) {
+      reject({ status: 500, statusText: error });
+    }
+  });
+}
+
 type TImageUploadProps = {
   openTrigger: ReactElement;
   entityType: string;
   entityId: string;
-  user: AuthStatus
+  user: AuthStatus;
 };
 
 type TFormErrors = {
@@ -40,10 +107,17 @@ type TImageMetdata = {
 const INITIAL_ERROR_STATE = {
   image: false,
   title: false,
-  author: false
+  author: false,
 };
 
-function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TImageUploadProps) {
+const ERROR_MESSAGE = "Prišlo je do napake pri shranjevanju fotografije.";
+
+function ImageUpload({
+  openTrigger,
+  entityType,
+  entityId,
+  user: authStatus,
+}: TImageUploadProps) {
   const router = useRouter();
   const { user, token } = authStatus;
   const [logDialogIsOpen, setLogDialogIsOpen] = useState(false);
@@ -54,22 +128,27 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
   const [formError, setFormError] = useState<TFormErrors>(INITIAL_ERROR_STATE);
   const imageInput = useRef<HTMLInputElement>(null);
   const titleRef = useRef<HTMLInputElement>(null);
+  const authorRef = useRef<HTMLInputElement>(null);
   const formRef = useRef<HTMLFormElement>(null);
 
-  const initialFormState = {
-    image: null,
-    title: "",
-    author: user !== undefined ? user.fullName : "",
-    entityId: entityId,
-    entityType: entityType
-  };
+  const initialFormState = useMemo(() => {
+    return {
+      image: null,
+      title: "",
+      author: user !== undefined ? user.fullName : "",
+      entityId: entityId,
+      entityType: entityType,
+    };
+  }, [entityId, entityType, user]);
 
-  const [formData, setFormData] = useState<TFormData>(initialFormState)
+  const [formData, setFormData] = useState<TFormData>(initialFormState);
   const [pickedImage, setPickedImage] = useState<string>();
-  const [pickedImageMetadata, setPickedImageMetadata] = useState<TImageMetdata>({
-    size: '',
-    name: ''
-  });
+  const [pickedImageMetadata, setPickedImageMetadata] = useState<TImageMetdata>(
+    {
+      size: "",
+      name: "",
+    }
+  );
 
   const handleImageChanged = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target && event.target.files && event.target.files.length > 0) {
@@ -81,11 +160,11 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
       setFormData({ ...formData, image: file });
       setPickedImageMetadata({ size: bytesToSize(file.size), name: file.name });
 
-      setFormError(prevState => {
+      setFormError((prevState) => {
         return {
           ...prevState,
-          image: false
-        }
+          image: false,
+        };
       });
       const fileReader: FileReader = new FileReader();
       fileReader.onload = () => {
@@ -95,18 +174,14 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
       };
       fileReader.readAsDataURL(file);
     }
-  }
+  };
   const handlePickClick = () => {
     if (imageInput.current !== null) {
       imageInput.current.click();
     }
-  }
-
-  const handleClose = () => {
-    setIsSubmitting(true);
   };
 
-  const resetForm = () => {
+  const resetForm = useCallback(() => {
     if (formRef.current !== null) {
       formRef.current.reset();
     }
@@ -114,7 +189,8 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
     setAuthorCheckbox(true);
     setFormData(initialFormState);
     setFormError(INITIAL_ERROR_STATE);
-  };
+    setError("");
+  }, [initialFormState]);
 
   const handleAuthorCheckboxClick = (value: boolean): void => {
     setAuthorCheckbox(value);
@@ -123,19 +199,16 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
       setFormError((prevState) => {
         const newState = {
           ...prevState,
-          author: false
+          author: false,
         };
         return newState;
       });
     }
-
-  }
+  };
 
   const onFileUploadProgressChanged = (percentage: number) => {
     setPercentage(percentage);
-  }
-
-  useEffect(() => setError(""), [logDialogIsOpen]);
+  };
 
   useEffect(() => {
     if (isSubmitting) {
@@ -143,72 +216,89 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
       const createImage = async (): Promise<void> => {
         try {
           const fd = new FormData();
-          fd.append('author', formData.author);
-          fd.append('title', formData.title);
-          fd.append('image', formData.image as Blob);
-          fd.append('entityId', formData.entityId);
-          fd.append('entityType', formData.entityType);
-          await createImageAction(token ?? '', fd, onFileUploadProgressChanged);
-          setLogDialogIsOpen(false);
-          resetForm();
-
-          router.refresh();
+          fd.append("author", formData.author);
+          fd.append("title", formData.title);
+          fd.append("image", formData.image as Blob);
+          fd.append("entityId", formData.entityId);
+          fd.append("entityType", formData.entityType);
+          createImageAction(token ?? "", fd, onFileUploadProgressChanged)
+            .then(() => {
+              setLogDialogIsOpen(false);
+              resetForm();
+              router.refresh();
+            })
+            .catch(() => {
+              setError(ERROR_MESSAGE);
+            })
+            .finally(() => {
+              setIsSubmitting(false);
+            });
         } catch (error) {
-          setError("Prišlo je do napake pri shranjevanju fotografije.");
-          resetForm();
+          setError(ERROR_MESSAGE);
+          setIsSubmitting(false);
         }
-        setIsSubmitting(false);
-      }
+      };
 
       const file: File | null = formData.image;
       const title: string = formData.title;
       const formAuthor: string = formData.author;
 
-      if (file === null || title.length == 0 || (formAuthor !== null && formAuthor.length == 0)) {
+      if (
+        file === null ||
+        title.length == 0 ||
+        (formAuthor !== null && formAuthor.length == 0)
+      ) {
         setFormError((prevState) => {
           const newState = {
             ...prevState,
             image: file === null,
             title: title.length == 0,
-            author: formAuthor !== null && formAuthor.length == 0
+            author: formAuthor !== null && formAuthor.length == 0,
           };
           return newState;
         });
         setIsSubmitting(false);
       } else {
-        if (formRef.current !== null) {
-          formRef.current.reset();
-        }
-        if (imageInput.current) {
-          imageInput.current.value = "";
-        }
-
         createImage();
-
-
       }
     }
-
-  }, [isSubmitting])
-
-
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    setIsSubmitting(true);
-    event.preventDefault();
-
-  }
+  }, [
+    formData.author,
+    formData.entityId,
+    formData.entityType,
+    formData.image,
+    formData.title,
+    isSubmitting,
+    resetForm,
+    router,
+    token,
+  ]);
 
   const handleTitleChange = async (value: string) => {
     if (titleRef.current !== null) {
       setFormData({ ...formData, title: value });
+      setFormError((prevState) => {
+        const newState = {
+          ...prevState,
+          title: value.length === 0,
+        };
+        return newState;
+      });
     }
-  }
+  };
 
-  const handleAuthorChange = async (value: string) => {
-    if (titleRef.current !== null) {
-      setFormData({ ...formData, author: value })
+  const handleAuthorChange = (value: string) => {
+    if (authorRef.current !== null) {
+      setFormData({ ...formData, author: value });
+      setFormError((prevState) => {
+        const newState = {
+          ...prevState,
+          author: value.length === 0,
+        };
+        return newState;
+      });
     }
-  }
+  };
 
   const resetImagePreview = () => {
     setPickedImage(undefined);
@@ -216,6 +306,10 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
     if (imageInput.current !== null) {
       imageInput.current.value = "";
     }
+  };
+
+  function handleClose(): void {
+    setIsSubmitting(true);
   }
 
   return (
@@ -238,8 +332,9 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
       }}
       isOpen={logDialogIsOpen}
       closeCallback={resetForm}
-      setIsOpen={setLogDialogIsOpen} >
-      <form ref={formRef} onSubmit={handleSubmit}>
+      setIsOpen={setLogDialogIsOpen}
+    >
+      <form ref={formRef}>
         {pickedImage && (
           <div className="flex flex-row justify-between">
             <div className="min-w-14 h-14 relative mb-6 mt-2">
@@ -255,57 +350,104 @@ function ImageUpload({ openTrigger, entityType, entityId, user: authStatus }: TI
               <div className="flex justify-between">
                 <div className="pt-1">
                   <span>
-                    {pickedImageMetadata.name}<br />
+                    {pickedImageMetadata.name}
+                    <br />
                   </span>
                   <span className="text-neutral-400">
-                    {pickedImageMetadata.size}<br />
+                    {pickedImageMetadata.size}
+                    <br />
                   </span>
                 </div>
                 <div>
-                  <Button disabled={isSubmitting} variant="quaternary" type="button" onClick={resetImagePreview}>
+                  <Button
+                    disabled={isSubmitting}
+                    variant="quaternary"
+                    type="button"
+                    onClick={resetImagePreview}
+                  >
                     <IconClose />
                   </Button>
                 </div>
-
               </div>
-              {isSubmitting && <div>
-                <ProgressBar value={percentage}></ProgressBar>
-              </div>}
+              {isSubmitting && (
+                <div>
+                  <ProgressBar value={percentage}></ProgressBar>
+                </div>
+              )}
             </div>
           </div>
         )}
         <input
           className="hidden"
-          type='file'
-          id='image'
-          accept='image/png, image/jpeg'
-          name='image'
+          type="file"
+          id="image"
+          accept="image/png, image/jpeg"
+          name="image"
           onChange={handleImageChanged}
-          ref={imageInput} />
-        {!pickedImage && <Button type="button" onClick={handlePickClick}>Izberi datoteko</Button>}
-        {formError?.image && <div className="mt-1 text-sm text-red-500" id="react-aria4183795666-:r7:">Izberite fotografijo</div>}
+          ref={imageInput}
+        />
+        {!pickedImage && (
+          <Button type="button" onClick={handlePickClick}>
+            Izberi datoteko
+          </Button>
+        )}
+        {formError?.image && (
+          <div
+            className="mt-1 text-sm text-red-500"
+            id="react-aria4183795666-:r7:"
+          >
+            Izberite fotografijo
+          </div>
+        )}
         <div className="mt-6 w-80">
-          <TextField name="title" ref={titleRef}
+          <TextField
+            name="title"
+            ref={titleRef}
             label="Naslov fotografije"
             isDisabled={isSubmitting}
             placeholder="npr. Pogled na steno"
             onChange={handleTitleChange}
-            errorMessage={(formError?.title) ? "Vpišite naslov fotografije" : undefined} />
+            errorMessage={
+              formError?.title ? "Vpišite naslov fotografije" : undefined
+            }
+          />
         </div>
         <div className="mt-6">
-          <Checkbox disabled={isSubmitting} checked={authorCheckbox} onChange={handleAuthorCheckboxClick} label={user?.gender === "F" ? "Sem avtorica fotografije" : "Sem avtor fotografije"} />
+          <Checkbox
+            disabled={isSubmitting}
+            checked={authorCheckbox}
+            onChange={handleAuthorCheckboxClick}
+            label={
+              user?.gender === "F"
+                ? "Sem avtorica fotografije"
+                : "Sem avtor fotografije"
+            }
+          />
         </div>
         <div className="mt-6 w-80">
           <TextField
             name="author"
             label="Avtor/ica fotografije"
-            isDisabled={(authorCheckbox && (user !== undefined)) || isSubmitting}
+            ref={authorRef}
+            isDisabled={(authorCheckbox && user !== undefined) || isSubmitting}
             placeholder="Ime in priimek avtorice oz. avtorja"
             onChange={handleAuthorChange}
             value={formData.author}
-            errorMessage={(formError?.author) ? "Vpišite avtorico oz. avtorja fotografije" : undefined} />
+            errorMessage={
+              formError?.author
+                ? "Vpišite avtorico oz. avtorja fotografije"
+                : undefined
+            }
+          />
         </div>
-        {error !== undefined && <div className="mt-4 text-sm text-red-500" id="react-aria4183795666-:r7:">{error}</div>}
+        {error !== undefined && (
+          <div
+            className="mt-4 text-sm text-red-500"
+            id="react-aria4183795666-:r7:"
+          >
+            {error}
+          </div>
+        )}
       </form>
     </Dialog>
   );
--- src/components/ui/progress-bar.tsx
+++ src/components/ui/progress-bar.tsx
@@ -8,59 +8,65 @@ interface ProgressBarProps {
 
 function ProgressBar({
   value,
-  size = 'default',
-  withLabelInside = false
+  size = "default",
+  withLabelInside = false,
 }: ProgressBarProps) {
-
-  let barStyles = 'bg-blue-500 rounded-full text-blue-100 text-center leading-none';
-  let frameStyles = 'bg-neutral-100 rounded-full w-full mt-2';
+  let barStyles =
+    "bg-blue-500 rounded-full text-blue-100 text-center leading-none";
+  let frameStyles = "bg-neutral-100 rounded-full w-full mt-2";
   const percentage = Math.round(value * 100);
   switch (size) {
-    case 'small':
-      barStyles += ' h-1.5';
-      frameStyles += ' h-1.5';
+    case "small":
+      barStyles += " h-1.5";
+      frameStyles += " h-1.5";
       break;
-    case 'large':
-      barStyles += ' h-4';
-      frameStyles += ' h-4';
+    case "large":
+      barStyles += " h-4";
+      frameStyles += " h-4";
       break;
-    case 'extra-large':
-      barStyles += ' h-6';
-      frameStyles += ' h-6';
+    case "extra-large":
+      barStyles += " h-6";
+      frameStyles += " h-6";
       break;
     default:
-      barStyles += ' h-2.5';
-      frameStyles += ' h-2.5';
+      barStyles += " h-2.5";
+      frameStyles += " h-2.5";
       break;
   }
   if (withLabelInside) {
-    if (size !== 'extra-large') { //reduce font size for inside label
-      barStyles += ' text-sm p-0.5';
+    if (size !== "extra-large") {
+      //reduce font size for inside label
+      barStyles += " text-sm p-0.5";
     } else {
-      barStyles += ' text-m p-1';
+      barStyles += " text-m p-1";
     }
   }
 
-
   const cssProperty: CSSProperties = {
-    width: `${value * 100}%`
+    width: `${value * 100}%`,
   };
 
   return (
     <>
-      {withLabelInside ? (<div className="flex">
-        <div className={frameStyles}>
-          <div className={barStyles} style={cssProperty}>{withLabelInside ? `${percentage}%` : ''}</div>
+      {withLabelInside ? (
+        <div className="flex">
+          <div className={frameStyles}>
+            <div className={barStyles} style={cssProperty}>
+              {withLabelInside ? `${percentage}%` : ""}
+            </div>
+          </div>
         </div>
-      </div>) : (<div className="flex">
-        <div className={frameStyles}>
-          <div className={barStyles} style={cssProperty}></div >
-        </div >
-        <div className={`ml-4 ${size === 'extra-large' ? 'pt-2' : 'pt-1'}`}>{`${percentage}%`}</div>
-      </div >)
-
-      }
+      ) : (
+        <div className="flex">
+          <div className={frameStyles}>
+            <div className={barStyles} style={cssProperty}></div>
+          </div>
+          <div
+            className={`text-sm ml-4 ${size === "extra-large" ? "pt-2" : "pt-1"}`}
+          >{`${percentage}%`}</div>
+        </div>
+      )}
     </>
   );
-};
+}
 export default ProgressBar;
