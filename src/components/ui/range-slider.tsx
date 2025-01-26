99856a1ec39e369943b27f0b9f5b0aef0e77b514
Anze Demsar <demshy@gmail.com>
2025-01-26 13:39:38 +0100

    format range slider

--- src/components/ui/range-slider.tsx
+++ src/components/ui/range-slider.tsx
@@ -18,7 +18,6 @@ interface SliderProps extends AriaSliderProps {
 
 function RangeSlider(props: SliderProps) {
   let trackRef = useRef(null);
-  
 
   let numberFormatter = useNumberFormatter(props.formatOptions);
   let state = useSliderState({ ...props, numberFormatter });
