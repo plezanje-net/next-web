0bc004e8efa48c74cc3e64f134fd99a4da2d5d42
Anze Demsar <demshy@gmail.com>
2025-11-23 18:58:31 +0100

    change home components from urql to gqlRequest

--- src/app/[lang]/components/latest-difficulty-votes.tsx
+++ src/app/[lang]/components/latest-difficulty-votes.tsx
@@ -5,27 +5,27 @@ import {
 } from "@/graphql/generated";
 import LatestDifficultyVote from "./latest-difficulty-votes/latest-difficulty-vote";
 import LatestDifficultyVoteSkeleton from "./latest-difficulty-votes/latest-difficulty-vote-skeleton";
-import urqlServer from "@/graphql/urql-server";
+import { gqlRequest } from "@/lib/graphql-client";
 
 async function LatestDifficultyVotes() {
-  const { data } = await urqlServer().query(HomeLatestDifficultyVotesDocument, {
-    input: {
-      pageSize: 10,
-    },
-  });
-
-  const difficultyVotes: DifficultyVote[] =
-    data?.latestDifficultyVotes.items ?? Array.from(new Array(10));
+  const { latestDifficultyVotes } = await gqlRequest(
+    HomeLatestDifficultyVotesDocument,
+    {
+      input: {
+        pageSize: 10,
+      },
+    }
+  );
 
   return (
     <>
       <h2>Zadnji predlogi ocen</h2>
       <ul>
-        {difficultyVotes.map((difficultyVote, index) =>
+        {latestDifficultyVotes?.items.map((difficultyVote, index) =>
           difficultyVote ? (
             <LatestDifficultyVote
               key={difficultyVote.id}
-              difficultyVote={difficultyVote}
+              difficultyVote={difficultyVote as DifficultyVote}
             />
           ) : (
             <LatestDifficultyVoteSkeleton key={index} />
@@ -35,36 +35,5 @@ async function LatestDifficultyVotes() {
     </>
   );
 }
-
-gql`
-  query HomeLatestDifficultyVotes($input: LatestDifficultyVotesInput!) {
-    latestDifficultyVotes(input: $input) {
-      items {
-        id
-        difficulty
-        created
-        route {
-          id
-          name
-          slug
-          difficulty
-          defaultGradingSystem {
-            id
-          }
-          crag {
-            id
-            name
-            slug
-          }
-        }
-        user {
-          id
-          fullName
-          gender
-        }
-      }
-    }
-  }
-`;
 
 export default LatestDifficultyVotes;
