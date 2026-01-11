3959ef08a1f66f6e531b1ac2fdf013e975590c2f
salamca <tomaz.bevk@gmail.com>
2026-01-11 18:10:24 +0100

    Fix map again.

--- src/components/map/map-marker.tsx
+++ src/components/map/map-marker.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import { TLazyMapMarkerProps as TMapMarkerProps } from "./lazy-map-marker";
 
--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import dynamic from "next/dynamic";
 import { TLazyMapProps as TMapProps } from "./lazy-map";
 
