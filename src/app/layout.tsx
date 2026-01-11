6ca3fac0d8e7f71a419400f5cfbe4397922429d9
Anze Demsar <demshy@gmail.com>
2026-01-11 16:23:27 +0100

    fix nuqs adapter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { Poppins } from "next/font/google";
 import { AuthProvider } from "@/lib/auth/auth-context";
 import { ReactNode } from "react";
 import getCurrentUser from "../lib/auth/get-current-user";
-import { NuqsAdapter } from 'nuqs/adapters/next/app'
+import { NuqsAdapter } from "nuqs/adapters/next/app";
 
 const poppins = Poppins({
   weight: ["400", "500"],
@@ -51,8 +51,9 @@ async function RootLayout({ children }: RootLayoutProps) {
       <body className={`text-neutral-900 ${poppins.className}`}>
         <AuthProvider currentUser={await getCurrentUser()}>
           <Header />
-          <NuqsAdapter>{children}</NuqsAdapter>
-          <main>{children}</main>
+          <NuqsAdapter>
+            <main>{children}</main>
+          </NuqsAdapter>
         </AuthProvider>
       </body>
     </html>
